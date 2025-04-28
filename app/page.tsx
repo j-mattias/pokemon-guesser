8bd535ffed6b5de960413b0938e4698fc2225744
Mattias <mattiasjohansson.vfx@gmail.com>
2025-04-28 16:36:36 +0200

    Add content to start page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import Link from "next/link";
+
 import "./page.css";
 
-export default function Home() {
+export default function Start() {
     return (
-        <main className="home">
-            <p>Start</p>
+        <main className="start">
+            <h1 className="start-title">Pokemon Guesser</h1>
+            <article className="instructions instructions--guesser">
+                <h2>Guess</h2>
+                <p>
+                    {`The game is based on the "Who's that Pokémon" segment from the Pokémon TV show. The siloutte of a Pokémon will be displayed. Type
+                    your guess into the input field*. If you guess correctly, hit the “Next” button
+                    to proceed to the next Pokémon. Guess all of them correctly to win.`}
+                </p>
+                <ol>
+                    <li>{`Select a Pokémon generation.`}</li>
+                    <li>{`Make your guess based on the Pokémon's silhoutte.`}</li>
+                </ol>
+                <small className="disclaimer">{`*Suggestions will be displayed based on what you type. Some Pokémon have special spellings, ex. Mr. Mime → Mr-Mime, use the suggestions to ensure the name is correct.`}</small>
+                <Link className="link-btn" href={`/guess`}>
+                    Play
+                </Link>
+            </article>
+            <article className="instructions instructions--pokedex">
+                <h2>Pokedex</h2>
+                <p>{`Browse all Pokémon or pick a generation. Click a Pokémon to get some additional information about that Pokémon.`}</p>
+                <Link className="link-btn" href={`/pokedex`}>
+                    Browse
+                </Link>
+            </article>
         </main>
     );
 }
