b5b81a7ff02ad86217579174fb9c3a23c0bf65d1
Mattias <mattiasjohansson.vfx@gmail.com>
2025-05-06 17:21:09 +0200

    Update start page and make it responsive.

--- app/page.tsx
+++ app/page.tsx
@@ -10,28 +10,38 @@ export default function Start() {
                 <Image className="start-logo" src={"/PG.svg"} alt="Pokemon Guesser logo" width={200} height={200} />
                 <h1 className="start-title">{`Pokémon Guesser`}</h1>
             </div>
-            <article className="instructions instructions--guesser">
-                <h2>Guess</h2>
-                <p>
-                    {`The game is based on the "Who's that Pokémon" segment from the Pokémon TV show. The siloutte of a Pokémon will be displayed. Type
-                    your guess into the input field*. If you guess correctly, hit the “Next” button
-                    to proceed to the next Pokémon. Guess all of them correctly to win.`}
-                </p>
-                <ol>
-                    <li>{`Select a Pokémon generation.`}</li>
-                    <li>{`Make your guess based on the Pokémon's silhoutte.`}</li>
-                </ol>
-                <small className="disclaimer">{`*Suggestions will be displayed based on what you type. Some Pokémon have special spellings, ex. Mr. Mime → Mr-Mime, use the suggestions to ensure the name is correct.`}</small>
-                <Link className="link-btn" href={`/guess`}>
-                    Play
-                </Link>
+            <article className="instructions">
+                <div className="instructions__desc">
+                    <h2>Guess</h2>
+                    <p>
+                        {`The game is based on the "Who's that Pokémon" segment from the Pokémon TV show. The siloutte of a Pokémon will be displayed. Type
+                        your guess into the input field*. If you guess correctly, hit the “Next” button
+                        to proceed to the next Pokémon. Guess all of them correctly to win.`}
+                    </p>
+                    <ol>
+                        <li>{`Select a Pokémon generation.`}</li>
+                        <li>{`Make your guess based on the Pokémon's silhoutte.`}</li>
+                    </ol>
+                    <small className="disclaimer">{`*Suggestions will be displayed based on what you type. Some Pokémon have special spellings, ex. Mr. Mime → Mr-Mime, use the suggestions to ensure the name is correct.`}</small>
+                    <Link className="link-btn" href={`/guess`}>
+                        Play
+                    </Link>
+                </div>
+                <figure className="instructions__fig">
+                    <Image className="instructions__img" src={`/PG_guesser.png`} alt="Image of Pokémon guess game" width={1920} height={1080} />
+                </figure>
             </article>
-            <article className="instructions instructions--pokedex">
-                <h2>Pokedex</h2>
-                <p>{`Browse all Pokémon or pick a generation. Click a Pokémon to get some additional information about that Pokémon.`}</p>
-                <Link className="link-btn" href={`/pokedex`}>
-                    Browse
-                </Link>
+            <article className="instructions">
+                <div className="instructions__desc">
+                    <h2>Pokedex</h2>
+                    <p>{`Browse all Pokémon or pick a generation. Click a Pokémon to get some additional information about that Pokémon.`}</p>
+                    <Link className="link-btn" href={`/pokedex`}>
+                        Browse
+                    </Link>
+                </div>
+                <figure className="instructions__fig">
+                    <Image className="instructions__img" src={`/PG_pokedex.png`} alt="Image of Pokédex" width={1920} height={1080} />
+                </figure>
             </article>
         </main>
     );
