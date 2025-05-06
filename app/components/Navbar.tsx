93ebade93c0052a475a67e8fa3c59029214d61df
Mattias <mattiasjohansson.vfx@gmail.com>
2025-05-06 16:20:18 +0200

    Responsive navbar.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,41 +1,70 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 import "./Navbar.css";
 
-const navLinks = [
+const NAV_LINKS = [
     { href: "/", name: "Start" },
     { href: "/guess", name: "Guess" },
     { href: "/pokedex", name: "Pokédex" },
 ];
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [navHeight, setNavHeight] = useState<number>(60);
+    const navRef = useRef<HTMLElement | null>(null)
     const pathname = usePathname();
 
+    // Close nav once user has navigated
+    useEffect(() => {
+        setIsOpen(false);
+    }, [pathname])
+
+    // Get the nav height to offset top pos (responsive)
+    useEffect(() => {
+        if (!navRef.current) return;
+
+        setNavHeight(navRef.current.clientHeight);
+    }, [])
+
     return (
-        <nav className="navbar">
+        <nav className={`navbar ${isOpen ? "fade-bg" : ""}`} ref={navRef}>
             <div className="nav-links">
                 <Link href={"/"} className="logo-link">
                     <Image src={"/PG.svg"} alt={"Pokemon Guesser logo"} width={40} height={40} />
                 </Link>
-                {navLinks.map((navLink) => {
-                    const isActive =
-                        navLink.href === pathname ||
-                        (pathname.startsWith(navLink.href) && navLink.href !== "/");
 
-                    return (
-                        <Link
-                            className={`nav-links__link ${isActive ? "active-link" : ""}`}
-                            href={navLink.href}
-                            key={navLink.name}
-                        >
-                            {navLink.name}
-                        </Link>
-                    );
-                })}
+                {/* Visible for small screens */}
+                <div className="menu-wrapper" onClick={() => setIsOpen((prevIO) => !prevIO)}>
+                    <div className={`nav-menu-btn`}>
+                        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+                        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+                        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+                    </div>
+                </div>
+
+                <div className={`nav-links__wrapper ${isOpen ? "show" : ""}`} style={{top: `${navHeight}px`}}>
+                    {NAV_LINKS.map((navLink) => {
+                        const isActive =
+                            navLink.href === pathname ||
+                            (pathname.startsWith(navLink.href) && navLink.href !== "/");
+
+                        return (
+                            <Link
+                                className={`nav-links__link ${isActive ? "active-link" : ""}`}
+                                href={navLink.href}
+                                key={navLink.name}
+                            >
+                                {navLink.name}
+                            </Link>
+                        );
+                    })}
+                </div>
             </div>
         </nav>
     );
