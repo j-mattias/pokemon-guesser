597308e13fca2e4fe30dae9453f87faafa8c78a0
Mattias <mattiasjohansson.vfx@gmail.com>
2025-05-14 19:54:01 +0200

    Update pokedex route loading file.

--- app/pokedex/loading.tsx
+++ app/pokedex/loading.tsx
@@ -3,12 +3,16 @@ import "./loading.css";
 export default function Loading() {
     return (
         <div className="loading">
-            <h1 className="loading__title">Pokedex</h1>
+            <h1 className="loading__title">{`Pokédex`}</h1>
+            <div className="search-loading-wrapper">
+                <div className="search-loading"></div>
+            </div>
             <div className="filters-loading">
                 {Array.from({ length: 9 }, (_, index) => index).map((item) => (
                     <div className="filters-loading__item" key={item}></div>
                 ))}
             </div>
+            <div className="filters-loading--small"></div>
             <div className="list-loading">
                 {Array.from({ length: 40 }, (_, index) => index).map((item) => (
                     <div className="list-loading__item" key={item}></div>
